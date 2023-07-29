b6508123f36ec14917b7a5a918e3fba54ea8086d
Katie Maison <kmm565@york.ac.uk>
2023-07-29 21:13:07 +0100

    Add imports

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,7 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
 export default function About() {
     return  (
     <Layout>
